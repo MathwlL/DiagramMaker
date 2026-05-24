ca7393750d65dd5fa979bd4cc13b9e5a62b66606
MathwlL <liramatheus720@gmail.com>
2026-05-24 16:28:25 -0300

    Added core files / constants/ parser / heading to opitmize site

---
+++ core/constants.py
@@ -0,0 +1,30 @@
+TABLE_W  = 170
+ROW_H    = 22
+HEADER_H = 30
+PAD      = 6
+
+FONT_HDR  = ("Segoe UI", 9, "bold")
+FONT_COL  = ("Segoe UI", 8)
+FONT_TYPE = ("Segoe UI", 7)
+FONT_CARD = ("Segoe UI", 8, "bold")
+
+COL_TYPES = [
+    "INT",
+    "VARCHAR(100)",
+    "VARCHAR(50)",
+    "VARCHAR(20)",
+    "TEXT",
+    "DECIMAL(10,2)",
+    "DATE",
+    "DATETIME",
+    "BOOLEAN",
+    "FLOAT",
+    "BIGINT",
+    "CHAR(1)"
+]
+
+DIAGRAM_MODES = [
+    "Conceitual",
+    "Logico",
+    "Fisico"
+]
